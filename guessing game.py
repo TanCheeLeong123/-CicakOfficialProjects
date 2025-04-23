4f46058f297cc6ffd6427e937ae2338819a62325
Tan Chee Leong aka emojiband2019 <103399069+TanCheeLeong123@users.noreply.github.com>
2025-04-23 09:15:57 +0800

    Add files via upload

---
+++ guessing game.py
@@ -0,0 +1,27 @@
+import random
+myname = input("What's your name? ")
+print("Hi, " + myname + ", I am thinking of a number between 100 to 999.")
+
+a = random.randint(100, 999)
+attempts = 0
+
+while True:
+    try:
+        guess = int(input("Guess your number from 100 to 999. "))
+        attempts += 1
+
+        if guess < 100 or guess > 999:
+            print("Please enter the number from 100 to 999.")
+        elif guess < a:
+            print("Too low! Try again.")
+        elif guess > a:
+            print("Too high! Try again.")
+        else:
+            print("You guessed the number " + str(a) + " in " + str(attempts) + " attempts.")
+            break
+        if attempts == 10:
+            print('Nope. The number I was thinking of was ' + str(a) + '.')
+            break
+    except ValueError:
+        print("Please enter a valid integer.")
+    
